46492ea70d9697b6992314d14da8ba288a3e7e51
Nandhakumar <kumarvvp7@gmail.com>
2019-01-29 19:18:22 +0530

    user update

--- src/app/signup/signup.service.ts
+++ src/app/signup/signup.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http'
+import {HttpClient} from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 
@@ -10,8 +10,12 @@ export class SignupService {
 
   constructor(private http: HttpClient) { }
 
-  saveUser(user: any): Observable<any>{
-    return this.http.post("/wow/saveuser", user);
+  saveUser(user: any): Observable<any> {
+    return this.http.post('/wow/api/user', user);
+  }
+
+  updateUser(user: any): Observable<any> {
+    return this.http.put('/wow/api/user', user);
   }
 
 }
--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -2,23 +2,31 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { UserService } from '../services/user.service';
 import { OrderService } from '../services/order.service';
+import { SignupService } from '../signup/signup.service';
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.css']
 })
-export class UserProfileComponent implements OnInit {
+export class UserProfileComponent {
 
   user: any = [];
   orders: any = [];
+  editable = false;
+  email: string;
+  phone: string;
+  noChange = false;
 
   constructor(private route: ActivatedRoute,
     private userService: UserService,
-    private orderService: OrderService) {
+    private orderService: OrderService,
+    private signupservice: SignupService) {
     let userId = route.snapshot.paramMap.get('id');
     userService.getUser(userId).subscribe(data => {
       this.user = data;
+      this.email = this.user.email;
+      this.phone = this.user.phone;
     });
     this.orderService.getMyOrders().subscribe(result => {
       this.orders = result;
@@ -26,7 +34,22 @@ export class UserProfileComponent implements OnInit {
     });
   }
 
-  ngOnInit() {
+  saveUserChanges() {
+    if (this.email === this.user.email && this.phone === this.user.phone) {
+      this.noChange = true;
+      console.log("no changed");
+    } else {
+      this.user.email = this.email;
+      this.user.phone = this.phone;
+      this.signupservice.updateUser(this.user).subscribe(
+        response => {
+            this.user = response;
+        },
+        error => {
+        }
+      );
+      this.noChange = false;
+    }
   }
 
 }
